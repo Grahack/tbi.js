35fba71382fbd48785b97cc389eacac2d39dbf37
Christophe Gragnic <christophegragnic@gmail.com>
2013-03-20 20:07:10 +0100

    Adapt canvas to doc size at init (first switch on).

--- sketch_helper.js
+++ sketch_helper.js
@@ -7,6 +7,8 @@ function sketch_reset() {
 }
 
 function sketch_switch() {
+    var canvas = $('#sketch').get(0);
+    if (canvas.width == 0) sketch_init();
     if ($('#sketch').hasClass('sketch-off')) {
         $('#sketch').removeClass('sketch-off').addClass('sketch-on');
         $('#sketch-switch').removeClass('sketch-switch-off').addClass('sketch-switch-on');
@@ -14,4 +16,16 @@ function sketch_switch() {
         $('#sketch').removeClass('sketch-on').addClass('sketch-off');
         $('#sketch-switch').removeClass('sketch-switch-on').addClass('sketch-switch-off');
     }
+}
+
+function sketch_init() {
+    // Detect doc size.
+    var db = document.body;
+    var dde = document.documentElement;
+    var docWidth = Math.max(db.scrollWidth, dde.scrollWidth, db.offsetWidth, dde.offsetWidth, db.clientWidth, dde.clientWidth);
+    var docHeight = Math.max(db.scrollHeight, dde.scrollHeight, db.offsetHeight, dde.offsetHeight, db.clientHeight, dde.clientHeight);
+    // Set canvas size accordingly.
+    var canvas = $('#sketch').get(0);
+    canvas.width = docWidth;
+    canvas.height = docHeight;
 }
