e383d71ab537a9115276082848951991192897ea
Christophe Gragnic <christophegragnic@gmail.com>
2013-03-20 08:16:34 +0100

    Switch button now fixed, switchs color.

--- sketch_helper.js
+++ sketch_helper.js
@@ -1,7 +1,9 @@
 function sketch_switch() {
     if ($('#sketch').hasClass('sketch-off')) {
         $('#sketch').removeClass('sketch-off').addClass('sketch-on');
+        $('#sketch-switch').removeClass('sketch-switch-off').addClass('sketch-switch-on');
     } else {
         $('#sketch').removeClass('sketch-on').addClass('sketch-off');
+        $('#sketch-switch').removeClass('sketch-switch-on').addClass('sketch-switch-off');
     }
 }
